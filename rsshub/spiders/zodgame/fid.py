b1651a2dc225ef651d588e8e6f1529ce42da037a
ddjcmc <120485917+ddjcmc@users.noreply.github.com>
2023-03-31 11:34:28 +0800

    Create fid.py
    fid for zodgame

---
+++ rsshub/spiders/zodgame/fid.py
@@ -0,0 +1,37 @@
+from rsshub.spiders.zodgame.zod_info import fid_model_dict,get_zod_module_url,cookie,proxies
+
+from parsel import Selector
+
+from rsshub.utils import DEFAULT_HEADERS,fetch
+
+
+
+DEFAULT_HEADERS.update({'cookie': cookie})
+
+domain = 'https://zodgame.xyz'
+
+def parse(post:Selector):
+    item = {}
+    item['title'] = post.css('.xst').xpath('.//text()').get()
+    item['link'] = domain+'/'+post.css('.xst').xpath('.//@href').get()
+    item['pubDate'] = post.xpath('.//td[2]/em//span/@title | .//td[2]/em//span/text()').extract_first()
+    
+    # 获取文章信息
+    res = fetch(url=item['link'], headers=DEFAULT_HEADERS,proxies=proxies)
+    item['description'] = res.css('.pcb').extract_first()
+    return item
+
+def ctx(fid:str|int,page:int|str=1):
+    # 获取 模块 html
+    url = get_zod_module_url(fid,page)
+    
+    res = fetch(url=url, headers=DEFAULT_HEADERS,proxies=proxies)
+    posts = res.css('tbody[id*="normalthread_"]')
+    
+    return  {
+        'title': f'Zodgame - {fid_model_dict[int(fid)]}',
+        'link': get_zod_module_url(fid),
+        'description': f'Zodgame - {fid_model_dict[int(fid)]}',
+        'author': 'mklwapc',
+        'items': list(map(parse, posts)) 
+    }
